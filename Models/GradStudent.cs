787dc7a9352abc3b72dc4587ea480bf6560a2740
TheAlchemistNerd <nevillemaloba@yahoo.com>
2025-05-30 00:39:44 +0300

    Saved the Changes made to GradStudent to include Guid Id

--- Models/GradStudent.cs
+++ Models/GradStudent.cs
@@ -5,17 +5,25 @@
         public bool FacultyAdvisor { get; set; }
         public bool TuitionCredit { get; set; }
 
-        public GradStudent(string firstName, string lastName, string emailAddress, 
+        public GradStudent(Guid id, string firstName, string lastName, string emailAddress, 
                            double gpa, bool facultyAdvisor, bool tuitionCredit)
-            : base(firstName, lastName, emailAddress, gpa)
+            : base(id, firstName, lastName, emailAddress, gpa)
         {
             FacultyAdvisor = facultyAdvisor;
             TuitionCredit = tuitionCredit;
         }
         
-        public GradStudent()
+        public GradStudent() : base()
         {
 
+        }
+
+        public GradStudent(string firstName, string lastName, string emailAddress,
+                           double gpa, bool facultyAdvisor, bool tuitionCredit)
+            : base(Guid.NewGuid(), firstName, lastName, emailAddress, gpa)
+        {
+            FacultyAdvisor = facultyAdvisor;
+            TuitionCredit = tuitionCredit;
         }
     }
 }
